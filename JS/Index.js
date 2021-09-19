41fb6194b6549c260aa3cc7d814b7ea50c10cf7b
etiqueludovic <etique.ludovic@gmail.com>
2021-09-19 10:27:35 +0200

    Create Index.js

---
+++ JS/Index.js
@@ -0,0 +1,21 @@
+class Produit{
+        constructor(jsonProduit){
+                jsonProduit && Object.assign(this, jsonProduit)
+        }   
+}
+
+let url = 'http://localhost:3000/api/teddies';
+
+fetch(url)
+    .then((response) => response.json())
+    .then((jsonListProduit) => {
+            for(let jsonProduit of jsonListProduit){
+                let produit = new Produit(jsonProduit);
+                document.querySelector(".produit").innerHTML += `<div class="vignette">
+                                                                        <img src="${jsonProduit.imageUrl}" />
+                                                                        <div class="name">${jsonProduit.name}</div>
+                                                                        <div class="price">${jsonProduit.price}</div>
+                                                                </div>`
+            }
+
+    });

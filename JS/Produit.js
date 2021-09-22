cec9be6b5ed8e166af581f884bbcf7b6f7e0c976
etiqueludovic <etique.ludovic@gmail.com>
2021-09-22 10:42:52 +0200

    Update Produit.js

--- JS/Produit.js
+++ JS/Produit.js
@@ -13,10 +13,10 @@ fetch(url2)
     .then((response) => response.json())
     .then((jsondata) => {
             console.log(jsondata);
-            for(let jsondonnee of jsondata){
-                let donnee = new Article(jsondonnee);
-                const price = `${(produit.price/100).toFixed(2)}`;
+            
+                let donnee = new Article(jsondata);
+                const price = `${(donnee.price/100).toFixed(2)}`;
                 document.querySelector("#image").innerHTML += `<img src="${donnee.imageUrl}"/>`;
                 document.querySelector("#name").textContent += `${donnee.name}`;
-                document.querySelector("#price").textContent +=  `${price}`;}
+                document.querySelector("#price").textContent +=  `${price}`;
     });

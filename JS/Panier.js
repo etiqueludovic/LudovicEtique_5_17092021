31905e12fd32830f0c87f22ac2497a3ce71e1f79
etiqueludovic <etique.ludovic@gmail.com>
2021-10-01 09:09:03 +0200

    Update Panier.js

--- JS/Panier.js
+++ JS/Panier.js
@@ -4,7 +4,9 @@ let recupstorage = JSON.parse(localStorage.getItem("produit"));
 
 // récupération total 
 let recuptotal = JSON.parse(localStorage.getItem("total"));
+var tot = 0;
 
+function affichage(){
 if(recupstorage == null){
     const paniervide = "Le panier est vide, veuillez ajouter des articles";
     document.querySelector("thead").innerHTML = "Le panier est vide, veuillez ajouter des articles";
@@ -14,9 +16,46 @@ if(recupstorage == null){
         document.querySelector("#conteneur-table").innerHTML += `<tr>
                    <td class="nom">${recupstorage[i].name_art}</td>
                    <td class="col">${recupstorage[i].col_art}</td>
-                   <td class="qty"><input type="number" value="${recupstorage[i].qty_art}"></input></td>
+                   <td><input type="number" class="qty" value="${recupstorage[i].qty_art}" onclick="modifqty"></td>
                    <td class="price">${recupstorage[i].price_art} €</td>
                    <td class="pricetotal">${recuptotal[i].pricetotal_art} €</td>
-               </tr>` 
-               console.log(recuptotal[i].pricetotal_art)            
+               </tr>`; 
+               tot += recuptotal[i].pricetotal_art;
+               var total = tot*(20/100);
+               var TTC = total+tot;
+               document.querySelector('#total').textContent = "Total HT :  " + tot + " €";
+               document.querySelector('#tva').textContent = "Total TVA :  " + total + " €";
+               document.querySelector('#TTC').textContent = "Total TTC :  " + TTC + " €";
+    
+                
+
+
 }};
+};
+let test = 0;
+let test2 = 0;
+function modifqty(){
+    test = document.getElementsByClassName('qty');
+    console.log(test)
+    for(j = 0;j < test.length; j++){
+    test2 += test[j].value;
+    document.querySelector('#tabTotal').innerHTML += "Total qty :  " + test2;
+    var valeurcourante = test[j].value;
+    if (parseInt(this.value) != valeurcourante){
+        let x = new Number(this.value);
+    }
+    valeurcourante = parseInt(x);
+    
+
+    };
+    
+      
+};
+
+
+affichage();
+modifqty();
+
+
+
+

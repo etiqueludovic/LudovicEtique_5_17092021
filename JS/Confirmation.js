bdd11902f4790b19affd2e45da2a965051f566f4
etiqueludovic <etique.ludovic@gmail.com>
2021-10-11 09:33:53 +0200

    Update Confirmation.js

--- JS/Confirmation.js
+++ JS/Confirmation.js
@@ -3,3 +3,13 @@ let total = JSON.parse(localStorage.getItem("totalorder"));
 
   document.querySelector("#totalcmd").textContent += total.totalTTC+"€";
   document.querySelector("#OrderId").textContent += contenu.orderId;
+ 
+ 
+  document.addEventListener('click', function(){
+  document.querySelector('#Confirmer').onclick=finish;
+});
+
+  function finish(){
+      localStorage.clear();
+      window.location.href = "index.html";
+};

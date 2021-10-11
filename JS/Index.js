806aef043243a710e58c56f6a1bb93bbd5378b2b
etiqueludovic <etique.ludovic@gmail.com>
2021-10-11 09:33:56 +0200

    Update Index.js

--- JS/Index.js
+++ JS/Index.js
@@ -1,3 +1,4 @@
+// création d'une classe
 class Produits{
         constructor(jsonProduits){
                 jsonProduits && Object.assign(this, jsonProduits)
@@ -5,15 +6,18 @@ class Produits{
 }
 
 
-
+// récupération du Back-end
 let url = 'http://localhost:3000/api/teddies';
 
 fetch(url)
+        // récupération des données en Json du Back-end
     .then((response) => response.json())
     .then((jsonListProduits) => {
                 for(let jsonProduits of jsonListProduits){
                         let produit = new Produits(jsonProduits);
+                        // on fixe le prix en euro et on le fixe à 2 chiffres en décimale
                         const price = `${(produit.price/100).toFixed(2)}`;
+                        // Remplace le corp de page par le suivant : (Ajout de chaque vignette par ID produit)
                         document.querySelector(".produit").innerHTML += `<a href="Produit.html?id=${produit._id}" class="vignette">
                                                                         <img src="${produit.imageUrl}" />
                                                                         <div class="desc">
@@ -24,14 +28,8 @@ fetch(url)
 
                                                                 `}        
     })
+    // Si le serveur 3000 n'est pas activé alors les articles ne s'affichent pas.
     .catch(function() {
         document.querySelector("#main").innerHTML = `<div>Le serveur local (port: 3000) n'est pas lancé</div>`;
-        //console.log("Le serveur local (port: 3000) n'est pas lancé"); // "zut !"
+        //console.log("Le serveur local (port: 3000) n'est pas lancé");
     });
-
-fetch(url)
-    .then((response) => response.json())
-    .then((data) => {
-            console.log(data);
-    });
-

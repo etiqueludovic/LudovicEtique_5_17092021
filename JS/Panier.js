37cd4c5e539279052e6df6b0126ebe7fe007759a
etiqueludovic <etique.ludovic@gmail.com>
2021-10-14 17:28:02 +0200

    Update Panier.js

--- JS/Panier.js
+++ JS/Panier.js
@@ -1,7 +1,11 @@
 // récupération du localStorage
 let produits = JSON.parse(localStorage.getItem("produits"));
+let totalorder = JSON.parse(localStorage.getItem("totalorder"));
+console.log("résultat produits")
+console.log(produits)
 
 var tot = 0;
+var pricetot = 0;
 
 function affichage(){
 if(produits == null){
@@ -13,10 +17,11 @@ if(produits == null){
         document.querySelector("#conteneur-table").innerHTML += `<tr id="${produits[i]._id+[i]}">
                    <td class="nom">${produits[i].name}</td>
                    <td class="col">${produits[i].colors}</td>
-                   <td class="qty${[i]}">${produits[i].qty}</td>
+                   <td><button class="btn_moin">-</button><span class="qty${[i]}">${produits[i].qty}</span><button class="btn_plus">+</button></td>
                    <td class="price">${produits[i].price} €</td>
-                   <td class="pricetotal">${produits[i].pricetotal} €</td>
-                   <td class="delete"><button class="btn-suppr">Supprimer</button></td>
+                   <td class="pricetotal${[i]}">${produits[i].pricetotal} €</td>
+                   <td class="delete"><button class="btn-suppr"><i class="fa fa-trash" aria-hidden="true"></i>
+                   </button></td>
                </tr>`;
                tot += produits[i].pricetotal;
                var totalHT = (tot-(tot*(20/100)));
@@ -49,12 +54,100 @@ function del(){
 
             localStorage.setItem("produits", JSON.stringify(produits));
             window.location.href = "panier.html";
+
         });
         
     };
 };
 
 del();
+
+
+function plus(){
+    let btn_plus = document.querySelectorAll(".btn_plus");
+
+    for (let k = 0; k < btn_plus.length; k++){
+        btn_plus[k].addEventListener("click", (event) =>{
+            event.preventDefault();
+            let selection_id_plus = produits[k]._id+produits[k].colors+[k];
+            console.log(selection_id_plus);
+
+            prod = produits.filter((el) => el._id+el.colors+[k] == selection_id_plus);
+            console.log(produits);
+
+            var qtyvalue = (produits[k].qty++)+1;
+            
+            document.querySelector(`.qty${[k]}`).innerHTML = `<span class="qty${[k]}">${qtyvalue}</span>`;
+            document.querySelector(`.pricetotal${[k]}`).innerHTML = `<td class="pricetotal${[k]}">${qtyvalue*produits[k].price} €</td>`;
+            tot += produits[k].price;
+            pricetot += tot;
+            console.log("price tot")
+            console.log(pricetot)
+            var totalHT = (tot-(tot*(20/100)));
+            var tva = (tot-totalHT);
+            document.querySelector('#total').textContent = "Total HT :  " + totalHT.toFixed(2) + " €";
+            document.querySelector('#tva').textContent = "Total TVA :  " + tva.toFixed(2) + " €"; 
+            document.querySelector('#TTC').textContent = "Total TTC :  " + tot.toFixed(2) + " €";
+            document.querySelector(".fa-shopping-cart").innerHTML = `<span id="qty">${produits[k].qty}</span>`;
+
+            produits.qty = qtyvalue; //Modifie la valeur
+            produits[k].pricetotal = produits[k].price*qtyvalue;
+            localStorage["produits"] = JSON.stringify(produits);  //store it again.
+
+            totalorder.totalTTC = tot;
+            localStorage["totalorder"] = JSON.stringify(totalorder);
+
+             console.log(localStorage["produits"], localStorage["totalorder"])
+
+
+
+        });
+        
+    };
+};
+
+plus();
+
+
+function moin(){
+    let btn_moin = document.querySelectorAll(".btn_moin");
+
+    for (let k = 0; k < btn_moin.length; k++){
+        btn_moin[k].addEventListener("click", (event) =>{
+            event.preventDefault();
+            let selection_id_moin = produits[k]._id+produits[k].colors+[k];
+            console.log(selection_id_moin);
+
+            prod = produits.filter((el) => el._id+el.colors+[k] == selection_id_moin);
+            console.log(produits);
+
+            var qtyvalue = (produits[k].qty--)-1;
+            document.querySelector(`.qty${[k]}`).innerHTML = `<span class="qty${[k]}">${qtyvalue}</span>`;
+            document.querySelector(`.pricetotal${[k]}`).innerHTML = `<td class="pricetotal${[k]}">${produits[k].qty*produits[k].price} €</td>`;
+            tot -= produits[k].price;
+            pricetot -= produits[k].pricetotal;
+            console.log(tot)
+            var totalHT = (tot-(tot*(20/100)));
+            var tva = (tot-totalHT);
+            document.querySelector('#total').textContent = "Total HT :  " + totalHT.toFixed(2) + " €";
+            document.querySelector('#tva').textContent = "Total TVA :  " + tva.toFixed(2) + " €"; 
+            document.querySelector('#TTC').textContent = "Total TTC :  " + tot.toFixed(2) + " €";
+            document.querySelector(".fa-shopping-cart").innerHTML = `<span id="qty">${produits[k].qty}</span>`;
+
+            produits.qty = qtyvalue; //Modifie la valeur
+            produits[k].pricetotal = produits[k].price*qtyvalue;
+            localStorage["produits"] = JSON.stringify(produits);  //store it again.
+
+            totalorder.totalTTC = tot;
+            localStorage["totalorder"] = JSON.stringify(totalorder);
+
+             console.log(localStorage["produits"], localStorage["totalorder"])
+        });
+        
+    };
+};
+
+moin();
 
 
 
@@ -134,3 +227,19 @@ function valider(e){
 
 var coordonnées = document.getElementById("coordonnées");
 coordonnées.addEventListener('submit', valider);
+
+var totqty = 0;
+function quantite(){
+    if (produits == "" || produits == undefined){
+        document.querySelector(".fa-shopping-cart").innerHTML = `<span id="qty">${totqty}</span>`;
+    }
+    else{
+    for(k = 0;k < produits.length; k++){
+    totqty += Number(produits[k].qty);
+        document.querySelector(".fa-shopping-cart").innerHTML = `<span id="qty">${totqty}</span>`;
+    
+}};
+console.log(totqty)
+};
+
+quantite();

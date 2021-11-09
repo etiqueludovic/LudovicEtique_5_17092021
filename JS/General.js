046c240f5b85e06943449d15376b6e904bca03e8
etiqueludovic <etique.ludovic@gmail.com>
2021-11-09 17:55:15 +0100

    Update General.js

--- JS/General.js
+++ JS/General.js
@@ -3,3 +3,23 @@ export class Article{
         jsondonnee && Object.assign(this, jsondonnee)
     }   
 }
+
+// récupération des information dnas la localStorage
+let produits = JSON.parse(localStorage.getItem("produits"));
+// variable avec comme valeur par defaut 0
+var totqty = 0;
+// fonction quantite qui sert à indiquer la quantité total du panier
+export function quantite(){
+    // si pas de tableau produits dans le localstorage on indique la valeur par defaut 0
+    if (produits == "" || produits == undefined){
+        document.querySelector(".fa-shopping-cart").innerHTML = `<span id="qty">${totqty}</span>`;
+    }
+    // sinon on indique la valeur total de quantité qui ce trouve dans le tableau produits
+    else{
+    for(let k = 0;k < produits.length; k++){
+        totqty += Number(produits[k].qty);
+        document.querySelector(".fa-shopping-cart").innerHTML = `<span id="qty">${totqty}</span>`;
+    
+        }   
+    };
+};

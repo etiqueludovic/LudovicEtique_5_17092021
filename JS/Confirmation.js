3dcbf77b1efb04fbb6e207dfbde10b153063dfd5
etiqueludovic <etique.ludovic@gmail.com>
2021-11-10 15:17:23 +0100

    Update Confirmation.js

--- JS/Confirmation.js
+++ JS/Confirmation.js
@@ -1,11 +1,12 @@
 // récupération des informations contenu dans le localstorage
 let contenu = JSON.parse(localStorage.getItem("contenu"));
 let total = JSON.parse(localStorage.getItem("totalorder"));
-// affichage du contenu voulu dans les champs adéquate
+// affichage du contenu voulu dans les champs total et numéro de commande
   document.querySelector("#totalcmd").textContent += total.totalTTC+"€";
   document.querySelector("#OrderId").textContent += contenu.orderId;
+  // en arrivant sur la page supprime tout le localstorage
   localStorage.clear();
- // bouton servant à supprimer tout le localstorage
+ // bouton servant à revenir à la page d'accueil
   document.querySelector('#Confirmer').addEventListener('click', function(){
     window.location.href = "index.html";
 });
